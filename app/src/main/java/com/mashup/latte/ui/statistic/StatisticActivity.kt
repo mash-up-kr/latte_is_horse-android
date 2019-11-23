32752f2fa031ec881e45800d0aef3f642fc614b6
SeongYunKim <cauboy17@naver.com>
2019-11-23 21:13:21 +0900

    Implement Statistic Activity UI

--- app/src/main/java/com/mashup/latte/ui/statistic/StatisticActivity.kt
+++ app/src/main/java/com/mashup/latte/ui/statistic/StatisticActivity.kt
@@ -1,5 +1,16 @@
 package com.mashup.latte.ui.statistic
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mashup.latte.R
+
 /**
  * Created by Namget on 2019.11.23.
  */
+class StatisticActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistic)
+    }
+}
