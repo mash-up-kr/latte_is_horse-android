b3535bdab5e94333d90e98d5bb6e9cf83754d91c
namget <yeboykr@gmail.com>
2019-11-24 04:21:43 +0900

    - main

---
+++ app/src/main/java/com/mashup/latte/data/DrunkCaffeine.kt
@@ -0,0 +1,6 @@
+package com.mashup.latte.data
+
+/**
+ * Created by Namget on 2019.11.23.
+ */
+data class DrunkCaffeine(val url : String, val name : String, val score : Int)
--- app/src/main/java/com/mashup/latte/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/latte/ui/main/MainActivity.kt
@@ -3,7 +3,11 @@ package com.mashup.latte.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.mashup.latte.R
+import com.mashup.latte.ui.main.adapter.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,24 +15,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
 
 
-    private fun init(){
-
+    private fun init() {
+        initViewPager()
+        initTabLayout()
     }
 
-    private fun initView(){
-        bottomAppbar.setOnMenuItemClickListener {
-            when(it.itemId){
 
-            }
+    private fun initViewPager() {
+        val fragmentList = ArrayList<Fragment>().apply {
+            add(DrunkFragment.newInstance())
+            add(QuestionFragment.newInstance())
+        }
 
+        container.apply {
+            adapter = MainViewPagerAdapter(supportFragmentManager, fragmentList)
+            offscreenPageLimit = 2
+            currentItem = 0
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater.inflate(R.menu.menu_bottom_app_bar,menu)
-        return true
+    private fun initTabLayout() {
+        tabLayout.setupWithViewPager(container)
     }
 }
+
