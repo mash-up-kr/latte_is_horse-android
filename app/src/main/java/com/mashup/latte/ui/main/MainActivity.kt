9948fdf1625888d8045642e2a461b3d12bad61a9
namget <yeboykr@gmail.com>
2019-11-23 17:22:04 +0900

    - error 수정

--- app/src/main/java/com/mashup/latte/ui/main/MainActivity.kt
+++ app/src/main/java/com/mashup/latte/ui/main/MainActivity.kt
@@ -2,12 +2,33 @@ package com.mashup.latte.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.mashup.latte.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    private fun init(){
+
+    }
+
+    private fun initView(){
+        bottomAppbar.setOnMenuItemClickListener {
+            when(it.itemId){
+
+            }
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater.inflate(R.menu.menu_bottom_app_bar,menu)
+        return true
     }
 }
